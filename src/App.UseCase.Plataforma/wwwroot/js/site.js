a86c30865007a2e8661bb23d0af65687c3c4aaf5
Abdias Ferreira <abdias.ferreira@contractor-mc1.com>
2024-09-30 14:01:36 -0300

    Desenvolvimento do use case

---
+++ src/App.UseCase.Plataforma/wwwroot/js/site.js
@@ -0,0 +1,35 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+
+
+
+
+ 
+
+$(document).ready(function () {
+
+
+    $("#divulgacao").click(function () {
+        
+        var requestUrl = '/Mensagem/CadastroModal';
+        $.get(requestUrl)
+           
+            .done(function (responsedata) {
+                console.log(requestUrl);
+
+                $("#partialViewContent").html(responsedata);
+
+                $('#myModal').modal('show')
+
+            })
+            .fail(function () {
+                alert("error");
+            })
+            .always(function () {
+                console.log("finished");
+            });
+    });
+});
